628f31936c86573a41dc06e711c44a67554e4633
Mukundan Gurumurthy <kailashguru129@gmail.com>
2024-08-15 20:33:18 +0530

    problem 5

--- pset0/strstr61.cc
+++ pset0/strstr61.cc
@@ -1,17 +1,33 @@
 #include <cstring>
 #include <cassert>
-#include <stdio.h>
+#include <cstdio>
 
 char* mystrstr(const char* s1, const char* s2) {                                         
-    // Your code here                                                                    
+    if (s1[0] == '\0') {
+        return (char *) s1;
+    }
+    for (auto i = 0; s1[i] != '\0'; i++) {
+        int check = 0;
+        if (s1[i] == s2[0]) {
+            for (auto j = 0; s2[j] != '\0'; j++) {
+                if (s2[j] != s1[i+j] && s2[j] != 0 && s1[i+j] != 0) {
+                    break;
+                }
+                check = j + 1;
+            }
+        }
+        if (s2[check] == '\0') {
+            return (char *) &s1[i];
+        }
+    }
     return nullptr;                                                                      
 }                                                                                        
                                                                                              
 int main(int argc, char* argv[]) {                                                       
     assert(argc == 3);                                                                   
-    printf("strstr(\"%s\", \"%s\") = %p\n",                                              
-           argv[1], argv[2], strstr(argv[1], argv[2]));                                  
-    printf("mystrstr(\"%s\", \"%s\") = %p\n",                                            
+    printf("strstr(\"%s\", \"%s\") = %p\n",
+           argv[1], argv[2], strstr(argv[1], argv[2]));                      
+    printf("mystrstr(\"%s\", \"%s\") = %p\n",
            argv[1], argv[2], mystrstr(argv[1], argv[2]));                                
     assert(strstr(argv[1], argv[2]) == mystrstr(argv[1], argv[2]));                      
 }
