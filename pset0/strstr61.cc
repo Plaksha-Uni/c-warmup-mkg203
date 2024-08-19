ed86515f5eff12c9f2683e0e92ae2414f8f69af6
Mukundan Gurumurthy <kailashguru129@gmail.com>
2024-08-19 14:29:13 +0530

    add part 2 problem 5

--- pset0/strstr61.cc
+++ pset0/strstr61.cc
@@ -22,6 +22,27 @@ char* mystrstr(const char* s1, const char* s2) {
     }
     return nullptr;                                                                      
 }                                                                                        
+
+char* mystrstr_ptr(const char* s1, const char* s2) {                                         
+    if (*s1 == '\0') {
+        return (char *) s1;
+    }
+    for (auto i = 0; *(s1+i) != '\0'; i++) {
+        int check = 0;
+        if (*(s1+i) == *s2) {
+            for (auto j = 0; *(s2+j) != '\0'; j++) {
+                if (*(s2+j) != *(s1+i+j) && *(s2+j) != 0 && *(s1+i+j) != 0) {
+                    break;
+                }
+                check = j + 1;
+            }
+        }
+        if (*(s2+check) == '\0') {
+            return (char *) &*(s1+i);
+        }
+    }
+    return nullptr;                                                                      
+}                                                                                        
                                                                                              
 int main(int argc, char* argv[]) {                                                       
     assert(argc == 3);                                                                   
@@ -30,4 +51,6 @@ int main(int argc, char* argv[]) {
     printf("mystrstr(\"%s\", \"%s\") = %p\n",
            argv[1], argv[2], mystrstr(argv[1], argv[2]));                                
     assert(strstr(argv[1], argv[2]) == mystrstr(argv[1], argv[2]));                      
+    printf("mystrstr_ptr(\"%s\", \"%s\") = %p\n",
+           argv[1], argv[2], mystrstr_ptr(argv[1], argv[2]));                                
 }
